5a860c8b09f5b6ba2dcd9b51cb49d89d1acd007e
Auston Pramodh Barboza <austonpramodh@gmail.com>
2020-09-21 20:48:21 +0530

    Cors Policies added

--- src/handler.ts
+++ src/handler.ts
@@ -1,12 +1,7 @@
 import urlJoin from "url-join";
-import { parseUrl } from "query-string";
 import isUrl from "is-url";
-import getCookie from "./utils/extractCookie";
+import { parseUrl, stringify } from "query-string";
 const fwdUrlKey = process.env.FORWARD_URL_KEY;
-const setCookieKey = "Set-Cookie";
-const cookieMaxAge = process.env.COOKIE_MAX_AGE;
-const defaultProxyUrl = process.env.DEFAULT_PROXY_URL;
-const isForwardedHeaderIndicatorKey = process.env.IS_FORWARDED_INDICATOR_KEY;
 
 const fetchResponse = async (url: string, { headers, method, body }: RequestInit): Promise<Response> => {
     return fetch(url, { method, headers, body });
@@ -14,29 +9,34 @@ const fetchResponse = async (url: string, { headers, method, body }: RequestInit
 
 export async function handleRequest(request: Request): Promise<Response> {
     try {
-        //parse params
-        //checking if undefined helps in avoiding addtion of undefined
-        //key in params of newly forming url
-        const params = request.url.split("?")[1] === undefined ? "" : request.url.split("?")[1];
-        //get the x-forward-url
-        let fwdUrl = request.headers.get(fwdUrlKey) || (parseUrl(request.url).query[fwdUrlKey] as string);
-
-        //check if x-forward-url is present, if not check for cookie
-        if (fwdUrl === undefined) {
-            const fwdCookieUrl = getCookie(request, fwdUrlKey);
-            fwdUrl = fwdCookieUrl || defaultProxyUrl;
-            // throw { errCode: 4001, errMsg: "ooops, you forgot to use x-forward-url for forwarding the request..." };
-        } else {
-            //check if x-forward-url is valid, if not throw error
-            if (!isUrl(fwdUrl)) throw { errCode: 4002, errMsg: "ooops, you gave a wrong url to forward..." };
+        if (request.method.toLowerCase() === "options") {
+            return new Response(null, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Headers": "*",
+                },
+            });
         }
+        //key in params of newly forming url
+        const queryParams = parseUrl(request.url).query;
+
+        //get the x-forward-url
+        const fwdUrl = request.headers.get(fwdUrlKey) || (queryParams[fwdUrlKey] as string | undefined);
+
+        //check if x-forward-url is present
+        if (!fwdUrl) throw { errCode: 4000, errMsg: "ooops, no forward url provided..." };
+        if (!isUrl(fwdUrl)) throw { errCode: 4001, errMsg: "ooops, you gave a wrong url to forward..." };
 
         // get path
-        const path = new URL(request.url).pathname;
+        const requestUrl = new URL(request.url);
+        const path = requestUrl.pathname;
+        //remove middleware params
+        delete queryParams[fwdUrlKey];
 
         //add path to fwdUrl and get final url
-        const finalFwdUrl = urlJoin(fwdUrl, path) + `?${params}`;
-
+        const stringifiedQueyParams = stringify(queryParams);
+        const finalFwdUrl = urlJoin(fwdUrl, path) + (stringifiedQueyParams ? `?${stringifiedQueyParams}` : "");
         //check if post method, if yes, append body to new request
         const requestMethod = request.method.toLocaleLowerCase();
         const requestBody =
@@ -50,10 +50,10 @@ export async function handleRequest(request: Request): Promise<Response> {
         });
         // Make the headers mutable by re-constructing the Response.
         const mutableFwdResponse = new Response(fwdResponse.body, fwdResponse);
-        //set the cookie so that subsequent requests get forwarded to same url
-        mutableFwdResponse.headers.set(setCookieKey, `${fwdUrlKey}=${fwdUrl}; Max-Age=${cookieMaxAge}`);
-        //set a header to indicate that the request is forwarded
-        mutableFwdResponse.headers.set(isForwardedHeaderIndicatorKey, "true");
+
+        //set the cors header.
+        mutableFwdResponse.headers.set("Access-Control-Allow-Origin", "*");
+        mutableFwdResponse.headers.set("Access-Control-Allow-Headers", "*");
         //return the whole response obtained
         return mutableFwdResponse;
     } catch (error) {
